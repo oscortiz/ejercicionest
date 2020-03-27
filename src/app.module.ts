11437ebee7ece11a8044a884915e709a3009b2ef
Óscar Ortiz <oscortiz@gmail.com>
2020-03-27 11:30:22 +0100

    Añadida configuración de acceso a BBDD en app.module.ts y verificada conexión con BBDD

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ApiModule } from './api/api.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ApiModule],
+  imports: [
+    ApiModule,
+    MongooseModule.forRoot(
+      'mongodb://localhost/test',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
